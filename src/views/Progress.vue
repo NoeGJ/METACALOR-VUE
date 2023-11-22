b81f488056f9f38f2c88466078df164e9a531048
NoeGJ <noargoji@gmail.com>
2023-11-22 14:29:34 -0600

    Date update

--- src/views/Progress.vue
+++ src/views/Progress.vue
@@ -11,7 +11,7 @@
           
         </v-col>
       <v-divider></v-divider>
-      <v-col cols="12" md="12"  justify="center">
+      <v-col cols="12" md="10" class="pa-lg-10"  justify="center">
         
           <v-card-text>           
               <Bar  :data="chartData" />            
@@ -21,11 +21,11 @@
       </v-row>
     </v-container>
     <v-divider></v-divider>
-    <v-container>
+    <v-container >
           <v-row justify="center">
-            <v-btn icon="mdi-less-than" class="" variant="plain">  </v-btn>
-            <p  class="text-center mt-3">{{ formattedDate }}</p>
-            <v-btn icon="mdi-greater-than" class="" variant="plain" v-if="hoy !== ultimoDia" @click="changeDate">  </v-btn>
+            <v-btn icon="mdi-less-than" class="" variant="plain" v-if="this.hoy.toLocaleDateString() !== this.primerDia.toLocaleDateString()" @click="changeDate(false)" > </v-btn>
+            <p   class="text-center mt-3 font-weight-medium">{{ hoy.toLocaleDateString() }}</p>
+            <v-btn icon="mdi-greater-than" class="" variant="plain" v-if="this.hoy.toLocaleDateString() !== this.ultimoDia.toLocaleDateString()" @click="changeDate(true)">  </v-btn>
           </v-row>
         <!-- <h3 class="text-center">{{ `${ dia } / ${ mes } / ${ anio } `   }}</h3> -->
 
@@ -45,15 +45,15 @@
 
 
 <script>
-import { Bar, Pie } from 'vue-chartjs'
-import { Chart as ChartJS, Title, Tooltip, ArcElement, Legend, BarElement, CategoryScale, LinearScale } from 'chart.js'
+import { Bar } from 'vue-chartjs'
+import { Chart as ChartJS, Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale } from 'chart.js'
 
-ChartJS.register(Title, Tooltip, Legend, ArcElement, BarElement, CategoryScale, LinearScale)
+ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale)
 
 
 export default {
   name: 'BarChart',
-  components: { Bar, Pie },
+  components: { Bar },
   
   data() {
     return {
@@ -66,7 +66,7 @@ export default {
         datasets: [
           {
             label: 'Calorias',
-            backgroundColor: ['#f87979'],
+            backgroundColor: ['#ff3030'],
             data: [40, 20, 12, 0,0,0,0]
           }
         ]
@@ -79,15 +79,16 @@ export default {
       }
     },
  methods:{
-        changeDate: function(){
-      //const primerDia = new Date( hoy.setDate( hoy.getDate() - hoy.getDay() ))
-      //const ultimoDia = new Date( hoy.setDate( hoy.getDate() - hoy.getDay() + 6 ))
+        changeDate: function(tipo){
       
-      this.hoy.setDate(this.hoy.getDate() + 1 )
-      this.$emit('update:hoy', new Date(this.hoy));
-      const formattedDate = `${this.hoy.getDate()} / ${this.hoy.getMonth()} / ${this.hoy.getFullYear()}`;
-    console.log('Nuevo valor de hoy:', formattedDate);
-
+          const fechaActual = new Date(this.hoy);      
+          tipo ? fechaActual.setDate(fechaActual.getDate() + 1) : fechaActual.setDate(fechaActual.getDate() - 1);
+    
+        //console.log(fechaActual)
+        this.hoy = fechaActual
+        //console.log(this.hoy)
+        //console.log(this.ultimoDia)
+        //console.log(this.hoy.toLocaleDateString() != this.ultimoDia.toLocaleDateString())
     }
   },
 
